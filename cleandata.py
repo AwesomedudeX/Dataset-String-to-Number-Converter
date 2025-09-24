6c4e47954b19232227b84dec2c56a8d8b9cbd841
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2025-09-24 12:55:48 -0600

    Update cleandata.py

--- cleandata.py
+++ cleandata.py
@@ -50,44 +50,58 @@ path = st.text_input("**Enter the dataset download URL here:**")
     
 if path != "":
     
-    df = pd.read_csv(path)
-
-    c1, c2 = st.columns(2)
-
-    with c1:
-
-        st.write("---")
-        st.header("Before:")
-        st.dataframe(df)
-
-    df, key = convert(df)
-
-    with c2:
-
-        st.write("---")
-        st.header("After:")
-        st.dataframe(df)
-        st.write("---")
-
-    write = f"Dataset URL: {path}"
-
-    if len(list(key.keys())) > 0:
-        
-        st.header("Value Key")
-
-        for k in key:
-
-            st.subheader(f"`{k}` Column:")
-            write += f"\n\n\n{k} Column"
-
-            for val in key[k]:
-                st.write(f"**{val}**: `{key[k][val]}`")
-                write += f"\n\n{val}: {key[k][val]}"
-
     st.sidebar.header("Download Files:")
 
-    if st.sidebar.download_button("Download Converted File", df.to_csv(), "data.csv"):
-        st.sidebar.success("Data downloaded successfully.")
+    try:
 
-    if st.sidebar.download_button("Download Conversion Key", data=write, file_name="conversion_key.txt"):
-        st.sidebar.success("Key downloaded successfully.")
+        df = pd.read_csv(path)
+
+        c1, c2 = st.columns(2)
+
+        with c1:
+
+            st.write("---")
+            st.header("Before:")
+            st.dataframe(df)
+
+        df, key = convert(df)
+
+        with c2:
+
+            st.write("---")
+            st.header("After:")
+            st.dataframe(df)
+            st.write("---")
+
+        write = f"Dataset URL: {path}"
+
+        if len(list(key.keys())) > 0:
+            
+            st.header("Value Key")
+
+            for k in key:
+
+                st.subheader(f"`{k}` Column:")
+                write += f"\n\n\n{k} Column"
+
+                for val in key[k]:
+                    st.write(f"**{val}**: `{key[k][val]}`")
+                    write += f"\n\n{val}: {key[k][val]}"
+
+        try:
+
+            if st.sidebar.download_button("Download Converted File", df.to_csv(), "data.csv"):
+                st.sidebar.success("Data downloaded successfully.")
+
+            if st.sidebar.download_button("Download Conversion Key", data=write, file_name="conversion_key.txt"):
+                st.sidebar.success("Key downloaded successfully.")
+
+        except:
+            st.sidebar.error("There was an error in downloading the file.")
+
+    except:
+
+        st.error("Please enter a valid dataset download URL.")
+
+else:
+    st.sidebar.header("Please enter a file download URL")
